67d5efc87b815ab1564b3f1d48d7ba6c4b4746fd
ruiz19746 <ruiz19746@iesmarenostrum.com>
2019-11-19 23:24:05 +0100

    commit Ventana Hija

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function mostrarDatos() {
     alert(nombreUsua + " " + contraUsua);
 }
 debugger
+
+var Ancho = screen.width;
+var Alto = screen.height;
+var A = Ancho * 50 / 100;
+var H = Alto * 50 / 100;
+var difA = Ancho - A;
+var difH = Alto - H;
+var tope = difH / 2;
+var lado = difA / 2;
+var Opciones = "status=no, menubar=no, directories=no, location=no, toolbar=no, scrollbars=yes, resizable=no, width=" + A + ", height=" + H + ", top=" + tope + ", left=" + lado + "";
+
+function registrarse() {
+    var vRegistro = window.open('E:/2º DAW/Entorno cliente/1 EVA/Proyecto integrado/ProyectoCliente/registro.html', 'Registrarse', Opciones);
+}
