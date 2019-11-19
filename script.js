62cb90863acf759804649a50461c65f4bf0c25e4
Ruiz19746 <ruiz19746@iesmarenostrum.com>
2019-11-19 18:46:36 +0100

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarDatos() {
+    var nombreUsua = document.sesion.usuario.value;
+    var contraUsua = document.sesion.contra.value;
+    alert(nombreUsua + " " + contraUsua);
+}
+debugger
